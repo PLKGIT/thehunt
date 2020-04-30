6de7334be19d0c779beeff31691b77f9a99aa58e
Lammie <plkelly@yahoo.com>
2020-04-30 15:03:45 -0700

    Configured react-bootstrap.

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,15 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Container from "./components/Container";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Container>
+      <div className="container">
         <h1 class="text-center">Hello Group 2!</h1>
-      </Container>
+      </div>
       <Footer />
     </div>
   );
