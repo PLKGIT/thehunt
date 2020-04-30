0be90cadc5be73858ac0d2c6fa9c2f340ec44269
Lammie <plkelly@yahoo.com>
2020-04-30 11:40:04 -0700

    Updated App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-   <div>
-     
-   </div>
+    <div>
+      <Navbar />
+      <Container>
+        <h1><center>Hello Group 2!</center></h1>
+      </Container>
+      <Footer />
+    </div>
   );
 }
 
