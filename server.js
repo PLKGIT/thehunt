2cf41b760e25504a55ed1f54c8a027d4404a871d
Lammie <plkelly@yahoo.com>
2020-04-30 12:46:43 -0700

    Updated server.js to include mongoose, axios, and passport settings.

--- server.js
+++ server.js
@@ -12,10 +12,40 @@ require('dotenv').config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+/*  Express Session  */
+const expressSession = require('express-session')({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(expressSession);
+
 /*  Static Assets  */
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname,'client/build')));
 }
+
+/*  Mongoose  */
+const mongoose = require('mongoose');
+
+/*  Axios  */
+const axios = require('axios');
+
+/*  Passport  */
+ const passport = require('passport');
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+/*  Passport Local Mongoose  */
+ const passportLocalMongoose = require('passport-local-mongoose');
+
+/*  MongoDB Connection  */
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/unlockit';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+/*  Models  */
+ const db = require('./models');
 
 /*  Server Port Configuration */
 const PORT = process.env.PORT || 3001;
