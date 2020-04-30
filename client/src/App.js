1bb597829f2719d4754991fccdec3d08d0ce95a5
Lammie <plkelly@yahoo.com>
2020-04-30 16:42:36 -0700

    Added text.

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Navbar fixed="top" className='bg-info'/>
       <Wrapper>
       <div className="container">
-        <h2 className="text-center"><img src="./images/unlock.gif" width="150px"/>&emsp;Hello Group 2!</h2>
+        <h2 className="text-center"><img src="./images/unlock.gif" width="150px"/>&emsp;Register | Login</h2>
       </div>
       </Wrapper>
       <Footer fixed="bottom"/>
