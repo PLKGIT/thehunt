38a79d136b2f8de699779261862bb320f800c521
Nida Memon <nidasmemon@gmail.com>
2020-05-11 18:54:09 -0700

    fixed homepage, navbar, create hunt page, footer, minor styling changes

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -4,6 +4,9 @@ import React from "react";
 /*  React Router  */
 import { Link } from "react-router-dom";
 
+/* Styling */
+import "../styles/gradient.scss";
+
 /*  Create NotFound  */
 function NotFound() {
   return (
@@ -11,8 +14,8 @@ function NotFound() {
       <br />
       <h2 className="text-center text-danger">Sorry, We Could Not Find That!</h2>
       <center><img src="./images/404.png" width="200px" alt="404" /></center>
-      <h5 className="text-info text-center">
-      <Link to="/"><img src="./images/unlock.png" alt="Still Lock" width="75px"/>Unlock It Home</Link>
+      <h5 className="text-info text-center back text-dark">
+      <Link to="/" className="link"><img src="./images/transparentlock.gif" alt="Still Lock" width="75px"/>Unlock It Home</Link>
         </h5>
       <br />
     </div>
