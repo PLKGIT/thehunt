c4b2c91b311281d90b7f03df32242e57e5713fd8
Pam Kelly <plkelly@yahoo.com>
2020-05-12 20:54:06 -0700

    Removed error in path for data.service file.

--- client/src/utils/data.service.js
+++ client/src/utils/data.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://unlock-it.herokuapp.com/api/auth/";
+const API_URL = "https://unlock-it.herokuapp.com/";
 
 class DataService {
     createOrg(org_name, org_city, org_state) {
